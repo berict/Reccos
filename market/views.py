18369dce1d00c8543d098b4d984d4ac46851f124
minjuho <joey91426@gmail.com>
2017-12-15 01:57:08 +0900

    add user(exception handling)

--- market/views.py
+++ market/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from .forms import ItemForm
 from .models import Item
 
-
 def main(request):
     items = Item.objects.all()  # order by latest
     return render(request, 'market/index.html', {'items': items})  # render to HTML
@@ -26,7 +25,10 @@ def item_new(request):
         if form.is_valid():
             item = form.save(commit=False)
             item.created_date = timezone.now()
-            item.user = User.objects.get(username='bedrockdev')
+            try:
+                item.user = User.objects.get(username='bedrockdev')
+            except :
+                item.user = User.objects.get(username='joey914')
             item.save()
             print('POST saved')
             return redirect('market:item_detail', pk=item.pk)
